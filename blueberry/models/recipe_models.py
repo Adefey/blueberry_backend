d32344f6423aac4cfeb5fe974f14b55824b1431c
Andrey Kolenkov <avoknelok@mail.ru>
2024-04-23 21:38:37 +0300

    step id is removed, we do not need step.id

--- blueberry/models/recipe_models.py
+++ blueberry/models/recipe_models.py
@@ -12,7 +12,6 @@ class StepForUI(BaseModel):
     caption: str = ""
     description: str = ""
     image_url: str = "https://adefe.xyz/avatar.png"
-    id: int = Field(0, ge=0)
 
 
 class RecipeForUI(BaseModel):
