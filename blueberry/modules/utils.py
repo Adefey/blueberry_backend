f29050908e4e7a3ca721a986ddf145056565aef6
Andrey Kolenkov <avoknelok@mail.ru>
2024-05-10 17:53:31 +0300

    fix no login and password check

--- blueberry/modules/utils.py
+++ blueberry/modules/utils.py
@@ -2,5 +2,7 @@ import re
 
 
 def validate_string(string: str) -> bool:
+    if len(string) < 5:
+        return False
     pattern = re.compile(r"[a-zA-Z0-9()!?@#^&$%_/.]*$")
     return bool(pattern.match(string))
